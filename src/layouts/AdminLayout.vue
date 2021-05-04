988d38a9cf24f3f981e3e91e8e78e641db2e7866
Francisco Rivera <franciscoriverap@gmail.com>
2021-05-04 11:01:30 -0300

    Added Audit Fiscal page and components

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -178,8 +178,8 @@ export default {
         { icon: 'business'  , text: this.$t('Empresas')  , to: { name: 'CompaniesIndex'    }},
       ],
       links2         : [
-        { icon: 'chevron_right', text: this.$t('Fiscal')        , to: { name: '' }},
-        { icon: 'chevron_right', text: this.$t('Contábil')      , to: { name: '' }},
+        { icon: 'chevron_right', text: this.$t('Fiscal')        , to: { name: 'AuditFiscalIndex'   }},
+        { icon: 'chevron_right', text: this.$t('Contábil')      , to: { name: 'AuditFiscalDetails' }},
         { icon: 'chevron_right', text: this.$t('Depto. Pessoal'), to: { name: '' }},
         { icon: 'chevron_right', text: this.$t('Societário')    , to: { name: '' }},
       ],
---
+++ src/pages/Audit/Fiscal/Index.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-page padding>
+    <q-card style="min-height: 90vh;">
+      <q-card-section>
+        <div class="row">
+          <div class="col-12 q-mb-md text-h6 text-left">
+            {{ $t('Auditoria fiscal') }}
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-tabs
+        align  ="justify"
+        v-model="currentTab"
+        class  ="bg-white text-primary"
+      >
+        <q-tab
+          name  ="impostos"
+          :label="$t('Impostos')"
+        />
+        <q-tab
+          name  ="declaracoes"
+          :label="$t('Declarações')"
+        />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels
+        v-model="currentTab"
+      >
+        <q-tab-panel name="impostos">
+          <AuditImpostos />
+        </q-tab-panel>
+
+        <q-tab-panel name="declaracoes">
+          <AuditDeclaracoes />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import AuditImpostos    from './../../../components/audit/AuditImpostos';
+import AuditDeclaracoes from './../../../components/audit/AuditDeclaracoes';
+
+export default {
+  name      : 'AuditFiscalIndex',
+
+  components: {
+    AuditImpostos   ,
+    AuditDeclaracoes,
+  },
+
+  computed: {
+    user() {
+      return this.$store.getters['auth/user'] || {};
+    },
+  },
+
+  data () {
+    return {
+      currentTab: 'impostos',
+    }
+  },
+
+  methods: {
+
+  },
+}
+</script>
