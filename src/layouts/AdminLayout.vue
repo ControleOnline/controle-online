fdc28bd0cb3613a6ea8686e1f6d0cb5057c1c3b7
Francisco Rivera <franciscoriverap@gmail.com>
2021-05-06 10:27:51 -0300

    Added Depto Declaracoes page and components

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -286,7 +286,7 @@ export default {
       ],
       links5         : [
         { icon: 'chevron_right', text: this.$t('Impostos')           , to: { name: 'DeptoImpostoIndex'     }},
-        { icon: 'chevron_right', text: this.$t('Declarações')        , to: { name: 'ContabilExtratosIndex' }},
+        { icon: 'chevron_right', text: this.$t('Declarações')        , to: { name: 'DeptoDeclaracaoIndex'  }},
         { icon: 'chevron_right', text: this.$t('Folhas de Pagamento'), to: { name: 'ContabilExtratosIndex' }},
         { icon: 'chevron_right', text: this.$t('Pro-Labore')         , to: { name: 'ContabilExtratosIndex' }},
         { icon: 'chevron_right', text: this.$t('Funcionários')       , to: { name: 'ContabilExtratosIndex' }},
