e0269353949fd2419b4c8eb633cab0ca74b6f9e3
Guilherme Smart Web <guilherme@smartwebsistemas.com.br>
2021-07-23 10:15:59 -0300

    reload if api return 401

--- src/boot/myapi.js
+++ src/boot/myapi.js
@@ -14,8 +14,10 @@ export const fetch = function(id, options = {}) {
   let myPromisse = myFetch(id, options);
 
   myPromisse.catch(e => {
-    if (e.message == 'Unauthorized')
+    if (e.message == 'Unauthorized' || e.message == 'Invalid credentials.')
       myStore.dispatch('auth/logOut');
+      localStorage.clear();
+      location.reload();
   });
 
   return myPromisse;
