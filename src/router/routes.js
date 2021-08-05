765a4c88293a7efb3e5b744402f4a01f9edb2ab2
Luiz Kim <luiz.kim@controleonline.com>
2021-08-04 21:25:59 -0300

    Tasks

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,13 @@
 import * as Login from '@controleonline/quasar-login-ui/src/router/routes';
 import * as Customers from '@controleonline/quasar-customers-ui/src/router/routes';
 import * as Providers from '@controleonline/quasar-providers-ui/src/router/routes';
+import * as Tasks from '@controleonline/quasar-providers-ui/src/tasks/routes';
 
 const routes = [
   ...Login.routes,
   ...Customers.routes,
   ...Providers.routes,
+  ...Tasks.routes,
   {
     path     : '/',
     component: () => import('layouts/MainLayout.vue'),
