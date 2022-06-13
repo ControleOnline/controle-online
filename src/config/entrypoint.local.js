b07d8e0f7c0c041cfab811496771432b3e102423
Luiz Kim <luiz.kim@controleonline.com>
2022-06-13 14:17:59 -0300

    Update

--- src/config/entrypoint.local.js
+++ src/config/entrypoint.local.js
@@ -1,1 +1,2 @@
-export const ENTRYPOINT_LOCAL = 'http://localhost:8000/';
+//export const ENTRYPOINT_LOCAL = 'http://localhost:8000/';
+export const ENTRYPOINT_LOCAL = 'https://api.controleonline.com/';
