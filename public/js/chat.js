e39225732b712d29daaad7dbb3aff5b378dbfb38
Luiz Kim <luiz.kim@controleonline.com>
2023-01-27 22:18:23 -0300

    Update

--- public/js/chat.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    let elem = document.createElement("div");
-   elem.classList.add("chat-tasks");
-   document.body.appendChild(elem);
-  });
