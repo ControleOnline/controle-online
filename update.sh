b2145cad038d1bc8d3473c42bb9cc974778f6b4c
Luiz Kim <luiz.kim@controleonline.com>
2023-02-16 12:41:35 -0300

    update

---
+++ update.sh
@@ -0,0 +1,8 @@
+npm install -g npm-check-updates
+ncu -u
+npm install --save vuex          --force
+npm install --save process       --force
+npm install --save stylus-loader --force
+npm install webpack@4.43.0 --save-dev
+npm install core-js@3.27.1 --save-dev
+npm install
