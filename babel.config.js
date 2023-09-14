135aa46b2398ee5e8307abb131d66e31db0b97ee
Luiz Kim <luiz.kim@controleonline.com>
2023-09-14 17:19:19 -0300

    Update

--- babel.config.js
+++ babel.config.js
@@ -13,9 +13,6 @@ module.exports = (api) => {
   return {
     presets: [
       "@quasar/babel-preset-app",
-      api.caller((caller) => caller && caller.target === "node")
-        ? { targets: { node: "current" } }
-        : {},
     ],
     plugins: [
       [
