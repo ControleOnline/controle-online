871d0d58af1cf1aea0cd7156f8dda43292389714
luizkim <luizkim@users.noreply.github.com>
2024-01-09 11:28:58 -0300

    Update api.js

--- src/boot/api.js
+++ src/boot/api.js
@@ -1,6 +1,5 @@
 import myFetch from "@controleonline/quasar-common-ui/src/utils/fetch";
 import axios from "axios";
-import { DOMAIN } from "../config/domain";
 
 let myStore = null;
 const MIME_TYPE = "application/ld+json";
@@ -8,29 +7,17 @@ export const api = {
   fetch: function (uri, options = {}) {
     if (typeof options.headers === "undefined")
       Object.assign(options, { headers: new Headers() });
-
     if (myStore.getters["auth/user"] && myStore.getters["auth/user"].token)
       options.headers.set("API-TOKEN", myStore.getters["auth/user"].token);
     options.headers.set("Content-Type", MIME_TYPE);
     options.headers.set("Accept", MIME_TYPE);
-    options.headers.set("app-domain", DOMAIN);
 
     if (options.body && typeof options.body != "string") {
       options.body = JSON.stringify(options.body);
     }
+    
     if (options.params) {
-      let params = [];
-
-      Object.keys(options.params).map((key) => {
-        if (Array.isArray(options.params[key])) {
-          params.push(
-            options.params[key].map((value) => `${key}[]=${value}`).join("&")
-          );
-        } else {
-          params.push(key + "=" + options.params[key]);
-        }
-      });
-      uri = `${uri}?${params.join("&")}`;
+      uri = this.buildQueryString(uri,options);            
     }
 
     return myFetch(uri, options).catch((e) => {
@@ -42,8 +29,41 @@ export const api = {
     });
   },
 
+   serialize(obj, prefix) {
+    const pairs = [];
+  
+    for (const key in obj) {
+      if (!obj.hasOwnProperty(key)) continue;
+  
+      const value = obj[key];
+      let fullKey = prefix ? `${prefix}[${key}]` : key;
+  
+      // Se o valor for um objeto, fazemos uma chamada recursiva
+      if (typeof value === "object" && value !== null) {
+        pairs.push(...serialize(value, fullKey));
+      } else if (Array.isArray(value)) {
+        // Tratamento específico para arrays
+        fullKey = `${fullKey}[]`;
+        value.forEach(val => {
+          pairs.push(`${encodeURIComponent(fullKey)}=${encodeURIComponent(val)}`);
+        });
+      } else {
+        // Para valores primitivos
+        pairs.push(`${encodeURIComponent(fullKey)}=${encodeURIComponent(value)}`);
+      }
+    }
+  
+    return pairs;
+  },
+  
+   buildQueryString(uri,options) {    
+    if (options.params) {
+      const params = this.serialize(options.params);
+      uri = `${uri}?${params.join("&")}`;
+    }
+    return uri;
+  },
   execute: function (params) {
-
     return axios(params);
   },
 };
