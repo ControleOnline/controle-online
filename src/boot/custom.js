3057a927a56d840ac32a899ab93ca8335b1478bc
Luiz Kim <luiz.kim@controleonline.com>
2024-02-08 10:42:41 -0300

    Update

--- src/boot/custom.js
+++ src/boot/custom.js
@@ -1,8 +1,16 @@
 import Contracts from "@controleonline/quasar-contracts-ui";
 import Dashboard from "@controleonline/quasar-dashboard-ui";
 import { boot } from "quasar/wrappers";
+import {
+  translate,
+  copyObject,
+} from "@controleonline/quasar-default-ui/src/components/Default/Scripts/DefaultMethods.js";
 
 export default boot(({ app }) => {
   app.use(Contracts);
   app.use(Dashboard);
+
+  app.config.globalProperties.$translate = (store, value, type) =>
+    translate(store, value, type);
+  app.config.globalProperties.$copyObject = (obj) => copyObject(obj);
 });
