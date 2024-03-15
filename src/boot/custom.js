16f723dea0f1958de5dd893ef9ef337798df58c9
Luiz Kim <luiz.kim@controleonline.com>
2024-03-15 16:07:06 -0300

    $formatter

--- src/boot/custom.js
+++ src/boot/custom.js
@@ -5,12 +5,15 @@ import {
   translate,
   copyObject,
 } from "@controleonline/quasar-default-ui/src/components/Default/Scripts/DefaultMethods.js";
+import Formatter from "@controleonline/quasar-common-ui/src/utils/formatter.js";
 
 export default boot(({ app }) => {
   app.use(Contracts);
   app.use(Dashboard);
 
+  app.config.globalProperties.$formatter = new Formatter();
   app.config.globalProperties.$translate = (store, value, type) =>
     translate(store, value, type);
   app.config.globalProperties.$copyObject = (obj) => copyObject(obj);
+
 });
