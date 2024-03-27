256c696cfd9f81ad02eb9f72ad1a2bd4da55bd0d
Luiz Kim <luiz.kim@controleonline.com>
2024-03-27 20:48:37 -0300

    Update

--- src/store/index.js
+++ src/store/index.js
@@ -23,6 +23,7 @@ import paymentType from "@controleonline/quasar-financial-ui/src/store/paymentTy
 import categories from "@controleonline/quasar-common-ui/src/store/categories";
 import status from "@controleonline/quasar-common-ui/src/store/status";
 import products from "@controleonline/quasar-products-ui/src/store/products";
+import productUnit from "@controleonline/quasar-products-ui/src/store/products/productUnit";
 import displays from "@controleonline/quasar-queues-ui/src/store/modules/displays";
 import queues from "@controleonline/quasar-queues-ui/src/store/modules/queues";
 import city from "@controleonline/quasar-common-ui/src/store/address/city";
@@ -58,6 +59,7 @@ export default store(function (/* { ssrContext } */) {
       menu,
       users,
       products,
+      productUnit,
       logistic,
       city,
       orders,
