a503941b35b5998526a1c2011c3dcb3e99c94b30
Luiz Kim <luiz.kim@controleonline.com>
2024-08-30 15:24:32 -0300

    Update

--- src/i18n/pt-br/index.js
+++ src/i18n/pt-br/index.js
@@ -4,58 +4,11 @@
 // This is just an example,
 // so you can safely delete all default props below
 
-import * as TranslateGlobal from "@controleonline/ui-common/src/i18n/pt-br/index"; // @Todo Gaffo =(^.^)=  (Atual principal)
-// import * as Login from "@controleonline/ui-legacy/ui-login/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Financial from "@controleonline/ui-financial/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Contracts from "@controleonline/ui-contracts/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Tasks from "@controleonline/ui-tasks/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Professionals from "@controleonline/ui-professionals/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Carriers from "@controleonline/ui-carrier/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Logistic from "@controleonline/ui-logistic/src/i18n/pt-br/index"; --> Enviado Para o principal
-<<<<<<< HEAD
-// import * as Coupon from "@controleonline/ui-legacy/ui-coupon/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Accounting from "@controleonline/ui-accounting/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Import from "@controleonline/ui-legacy/ui-import/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as CRM from "@controleonline/ui-crm/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as People from "@controleonline/ui-people/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Docs from "@controleonline/ui-legacy/ui-docs/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Config from "@controleonline/ui-config/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Products from "@controleonline/ui-products/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Queues from "@controleonline/ui-queues/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Default from "@controleonline/ui-default/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Logistic_old from "@controleonline/ui-legacy/ui-logistic/src/i18n/pt-br/index"; --> Enviado Para o principal
-// import * as Orders from "@controleonline/ui-orders/src/i18n/pt-br/index"; --> Enviado Para o principal
-=======
-import * as Coupon from "@controleonline/ui-legacy/ui-coupon/src/i18n/pt-br/index";
-import * as Accounting from "@controleonline/ui-accounting/src/i18n/pt-br/index";
-import * as CRM from "@controleonline/ui-crm/src/i18n/pt-br/index";
-import * as People from "@controleonline/ui-people/src/i18n/pt-br/index";
-import * as Docs from "@controleonline/ui-legacy/ui-docs/src/i18n/pt-br/index";
-import * as Config from "@controleonline/ui-config/src/i18n/pt-br/index";
-import * as Products from "@controleonline/ui-products/src/i18n/pt-br/index";
-import * as Queues from "@controleonline/ui-queues/src/i18n/pt-br/index";
-import * as Default from "@controleonline/ui-default/src/i18n/pt-br/index";
-import * as Logistic_old from "@controleonline/ui-legacy/ui-logistic/src/i18n/pt-br/index";
-import * as Orders from "@controleonline/ui-orders/src/i18n/pt-br/index";
->>>>>>> 3053169e03f8ed78393509d9b60c98192884180a
+import * as TranslateGlobal from "@controleonline/ui-common/src/i18n/pt-br/index";
 
 export default {
   app: {
     name: "Controle Online",
   },
-<<<<<<< HEAD
-=======
-  ...People.translate,
-  ...Accounting.translate,
-  ...Coupon.translate,
-  ...CRM.translate,
-  ...Docs.translate,
-  ...Config.translate,
-  ...Products.translate,
-  ...Queues.translate,
-  ...Default.translate,
-  ...Logistic_old.translate,
-  ...Orders.translate,
->>>>>>> 3053169e03f8ed78393509d9b60c98192884180a
   ...TranslateGlobal.translate,
 };
