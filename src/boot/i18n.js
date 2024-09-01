88aaa534b824d6434a343f6eed545ac0fda6066e
Luiz Kim <luiz.kim@controleonline.com>
2024-09-01 18:22:18 -0300

    Update

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,12 +1,11 @@
 import { boot } from "quasar/wrappers";
 import { createI18n } from "vue-i18n";
-import messages from "src/i18n";
 
 export const i18n = createI18n({
   locale: "pt-br",
   fallbackLocale: "en-us",
   globalInjection: true,
-  messages,
+  messages: {},
 });
 
 export default boot(({ app }) => {
--- src/i18n/index.js
+++
@@ -1,5 +0,0 @@
-
-export default {
-  "pt-br":{},
-  "en-us":{}
-};
