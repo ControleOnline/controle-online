faab7cec04f4d748b71a84bb2dd088d01c84ee59
Luiz Kim <luiz.kim@controleonline.com>
2024-09-03 14:17:04 -0300

    Update

--- src/boot/custom.js
+++ src/boot/custom.js
@@ -1,5 +1,8 @@
 import { boot } from "quasar/wrappers";
+import DefaultTable from "@controleonline/ui-default/src/components/Default/DefaultTable";
 
 
 export default boot(({ app }) => {
+    app.component('DefaultTable', DefaultTable);
+
 });
