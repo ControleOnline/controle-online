8d1647597326821cbf8f3bd3b2d3ee2dd6de57c0
BrunoGaffo <67167417+BrunoGaffo@users.noreply.github.com>
2024-09-17 14:47:44 -0300

    Update

---
+++ modules/controleonline/ui-translate/src/pages/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <Translate />
+
+</template>
+
+<script>
+import Translate from "@controleonline/ui-translate/src/components/Translate/Table";
+
+export default {
+  //
+  components: {
+    Translate,
+  },
+
+  computed: {
+
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {},
+};
+</script>
