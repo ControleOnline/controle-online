89e819186bc5077371cbe8dd5dfa3132736e5956
Luiz Kim <luiz.kim@controleonline.com>
2024-09-29 21:07:49 -0300

    Update

--- src/boot/custom.js
+++ src/boot/custom.js
@@ -1,14 +1,19 @@
 import { boot } from "quasar/wrappers";
 import DefaultTable from "@controleonline/ui-default/src/components/Default/DefaultTable";
 import DefaultForm from "@controleonline/ui-default/src/components/Default/Common/DefaultForm";
+import File from "@controleonline/ui-default/src/components/Default/Common/Inputs/File.vue";
+import FileExplorer from "@controleonline/ui-common/src/components/Common/FileExplorer";
 
 export default boot(({ app }) => {
-    app.component('DefaultTable', DefaultTable);
-    app.component('DefaultForm', DefaultForm);
+  app.component("DefaultTable", DefaultTable);
+  app.component("FileExplorer", FileExplorer);
+  app.component("DefaultForm", DefaultForm);
+  app.component("File", File);
 
-
-    app.config.globalProperties.$components = {
-        DefaultTable,
-        DefaultForm,
-      };
+  app.config.globalProperties.$components = {
+    DefaultTable,
+    FileExplorer,
+    DefaultForm,
+    File,
+  };
 });
