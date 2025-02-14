d05f0d6267000052de6a5e8ccc952d7554bfdb43
Luiz Kim <luiz.kim@controleonline.com>
2025-02-13 21:06:09 -0300

    Update

--- src/boot/custom.js
+++ src/boot/custom.js
@@ -5,6 +5,7 @@ import File from "@controleonline/ui-default/src/components/Default/Common/Input
 import FileExplorer from "@controleonline/ui-common/src/components/Common/FileExplorer";
 import DefaultButtonDialog from "@controleonline/ui-default/src/components/Default/DefaultButtonDialog";
 import DefaultComponent from "@controleonline/ui-default/src/components/Default/DefaultComponent.vue";
+import DefaultInput from "@controleonline/ui-default/src/components/Default/DefaultInput.vue";
 
 export default boot(({ app }) => {
   app.component("DefaultTable", DefaultTable);
@@ -13,11 +14,13 @@ export default boot(({ app }) => {
   app.component("DefaultForm", DefaultForm);
   app.component("File", File);
   app.component("DefaultComponent", DefaultComponent);
+  app.component("DefaultInput", DefaultInput);
 
   app.config.globalProperties.$components = {
     DefaultTable,
     DefaultComponent,
     DefaultButtonDialog,
+    DefaultInput,
     FileExplorer,
     DefaultForm,
     File,
